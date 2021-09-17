113987e07cc8b18f546fc226af7843ef197b1e68
NMITTutor <Todd.Cochrane@nmit.ac.nz>
2021-09-17 12:29:06 +1200

    Add files via upload

---
+++ controller/DES/open_csv.py
@@ -0,0 +1,25 @@
+"""
+OpenCSV button controller
+"""
+import sys
+sys.dont_write_bytecode = True
+import PySimpleGUI as sg
+from model.model import Model
+
+def accept( event, values, state):
+    
+    keep_going = True
+    if event == 'Open CSV':
+       
+        
+        file_name = sg.PopupGetFile('Please select file to open', file_types=(("Comma separated value", "*.csv"),)) 
+        if file_name != None :
+            
+            view = state['view']
+            model = Model(data_source = file_name)
+            fin_liabilities = model.get_column('Household financial liabilities (including rental properties) as a percentage of household disposable income')
+            stats_months = model.get_column('Year')
+            view.update_current_data(values,file_name,data=fin_liabilities,x_values=stats_months,y_values=fin_liabilities,
+                                     x_label='Year Month',y_label='House Fin Liability %',title_label='Household financial liabilites % of disposable income')
+
+    return keep_going
