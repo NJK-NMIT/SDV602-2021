8079b356262b4809222a039e775fad1d81901149
NMITTutor <Todd.Cochrane@nmit.ac.nz>
2021-09-19 13:41:02 +1200

    Change to view D3. Total interest payments

--- controller/DES/open_csv.py
+++ controller/DES/open_csv.py
@@ -17,9 +17,9 @@ def accept( event, values, state):
             
             view = state['view']
             model = Model(data_source = file_name)
-            fin_liabilities = model.get_column('Household financial liabilities (including rental properties) as a percentage of household disposable income')
+            fin_liabilities = model.get_column('    D3. Total interest payments as a percentage of household disposable income')
             stats_months = model.get_column('Year')
             view.update_current_data(values,file_name,data=fin_liabilities,x_values=stats_months,y_values=fin_liabilities,
-                                     x_label='Year Month',y_label='House Fin Liability %',title_label='Household financial liabilites % of disposable income')
+                                     x_label='Year Month',y_label='Interest %',title_label='Interest payments % of disposable income')
 
     return keep_going
