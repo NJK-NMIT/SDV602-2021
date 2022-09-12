2c67f10e430512df3cb6e5c0b664ae8b590c1d01
Nick Kean <109487676+NJK-NMIT@users.noreply.github.com>
2022-09-12 12:45:41 +1200

    Forked my own copy for editing
    Forked my own copy from https://github.com/NMITTutor/SDV602-2021

--- data_explorer.py
+++ data_explorer.py
@@ -31,15 +31,15 @@ if __name__ == "__main__" :
     """
     Code that runs when this is the main module.
     """
-    #des_obj = DES_View()
-    #des_obj.set_up_layout()
-    #des_obj.render()
+    des_obj = DES_View()
+    des_obj.set_up_layout()
+    des_obj.render()
     
-    #des_obj.accept_input()
+    des_obj.accept_input()
 
-    login_view = LoginView()
-    login_view.set_up_layout()
-    login_view.render()
-    login_view.accept_input()
+    #login_view = LoginView()
+    #login_view.set_up_layout()
+    #login_view.render()
+    #login_view.accept_input()
 
     pass
